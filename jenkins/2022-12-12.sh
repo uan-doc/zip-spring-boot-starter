75395544566a120930ca9bd20c25b67e24d34df0
Jesus Saad <jesus.saad1@gmail.com>
2022-12-16 14:49:46 -0300

    novo jenkins

--- jenkins/2022-12-12.sh
+++ jenkins/2022-12-12.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-cd /Users/Shared/ON3/git/devops/jenkins/api || exit
-./script_clone_job.sh s3-spring-boot-starter zip-spring-boot-starter false
-./add_job_to_view.sh 'zip-spring-boot-starter' 'GedocFlex - MicroServices'
-./build.sh 'zip-spring-boot-starter'
+/Users/Shared/ON3/git/devops/jenkins/api/script_clone_default_build_job.sh 'zip-spring-boot-starter' 'Infra'
