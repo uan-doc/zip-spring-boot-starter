165971c8f30f1f8ee4ee6b1a8d1b7216987b3a8a
Jesus Saad <jesus.saad1@gmail.com>
2022-12-22 13:12:25 -0300

    novo jenkins

--- jenkins/2022-12-12.sh
+++ jenkins/2022-12-12.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+/Users/Shared/ON3/git/devops/github/add_web_hook.sh uan-doc zip-spring-boot-starter
 /Users/Shared/ON3/git/devops/jenkins/api/script_clone_default_build_job.sh 'zip-spring-boot-starter' 'Infra'
